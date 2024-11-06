93f03dabc3d365c8e5afa342b8259ec50753aa76
meharden438 <meharden@mtu.edu>
2024-11-05 20:30:30 -0500

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -5,14 +5,14 @@ drop table if exists Category;
 drop table if exists Customer;
 drop table if exists Orders;
 drop table if exists Order_Item;
-drop table if exists Epdates;
+drop table if exists Updates;
 drop table if exists Makes;
 drop table if exists In_Cart;
 drop table if exists Belongs_To;
 
 #emplyoee table
 create table Employee(
-	employee_ID char(4) primary key,
+	employee_id char(4) primary key,
     username varchar(30),
     email varchar(30),
     passwrd varchar(30) not null
@@ -20,42 +20,64 @@ create table Employee(
 
 #customer table
 create table Customer(
-	c_ID char(4) primary key,
-    user varchar(30),
-    passwrd varchar(250),
-    f_name varchar(30),
-    l_name varchar(30),
-    email varchar(100),
-    address varchar(250)
+	c_id char(4) primary key,
+    user varchar(30) unique,
+    passwrd varchar(250) not null,
+    f_name varchar(30) not null,
+    l_name varchar(30) not null,
+    email varchar(100) not null,
+    address varchar(250) not null
 );
 
 #catergory table
 create table Catergory(
 	cat_Name varchar(30) primary key,
-    cat_Desc varchar(200)
+    cat_Desc varchar(200) not null
 );
 
 #product table
 create table Product(
-	product_ID char(4) primary key,
-    product_name varchar(30),
-    product_desc varchar(250),
-    price numeric(5,2),
-	adv_stock int,
-    act_stock int,
+	product_id char(4) primary key,
+    product_name varchar(30) not null,
+    product_desc varchar(250) not null,
+    price numeric(5,2) not null,
+	adv_stock int not null,
+    act_stock int not null,
     discont bool,
-    category varchar(30),
+    category varchar(30) not null,
     image varchar(250),
     foreign key (category) references Category(cat_Name)
 );
 
+#order table
 create table Orders(
-	order_ID char(4) primary key,
-    c_ID char(4),
-	order_Date date,
-    order_Status varchar(250),
-    total numeric(10,2)
+	order_id char(4) primary key,
+    c_id char(4) not null,
+	order_Date date not null,
+    order_Status varchar(250) not null,
+    total numeric(10,2) not null
 );
+
+#order_items table
+create table order_Items(
+	order_id char(4) primary key,
+    product_id char(4),
+    quanity int,
+    price numeric(5,2),
+    foreign key (product_id) references Product(product_id),
+    foreign key (price) references Product(price)
+);
+
+#update table
+create table Updates(
+	employee_id char(4) primary key,
+    product_id char(4),
+	up_Time date not null,
+    desc_of_uodate varchar(250),
+    foreign key (employee_id) references Employee(employee_id),
+    foreign key (product_id) references Product(product_id)
+);
+
 
 
     
